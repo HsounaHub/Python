a48416d2858bca0fa33d62bed1f004c7ec6b9493
HsounaHub <hsounaali10@gmail.com>
2023-05-16 15:41:07 +0100

    Assignment: User

---
+++ fundamentals/oop/user.py
@@ -0,0 +1,34 @@
+class user:
+    def __init__(self,first_name,last_name,email,age,is_rewards_member=False,gold_card_points = 0):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.email=email
+        self.age=age
+        self.is_rewards_member=is_rewards_member
+        self.gold_card_points=gold_card_points
+    def display_info(self):
+        print(self.first_name)
+        print(self.last_name)
+        print(self.email)
+        print(self.age)
+        print(self.is_rewards_member)
+        print(self.gold_card_points)
+    def enroll(self):
+        if not self.is_rewards_member :
+            self.is_rewards_member=True
+            self.gold_card_points=200
+            return True
+        if self.is_rewards_member :
+            print("User already a member.")
+    def spend_points(self, amount):
+        if self.gold_card_points - amount >=0:
+            self.gold_card_points=self.gold_card_points - amount
+            print(f"your have now {self.gold_card_points} poins")
+        if self.gold_card_points - amount <0:
+            print("you don't have enough points" )
+aymen=user("aymen","ben moussa","aymen@gmail.com",24,True,8)
+#aymen.display_info()
+ali=user("ali","hsouna","ali@gmail.com",28,False)
+aziz=user("aziz","med saleh","aziz@gmail.com",19,True,300)
+ali.enroll()
+ali.display_info()
